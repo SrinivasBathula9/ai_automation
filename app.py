ec531a153165977f822ae939319fceb8b620a03f
Srinivas <65796687+SrinivasBathula9@users.noreply.github.com>
2025-02-06 18:14:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,32 +8,32 @@ import pandas as pd
 # Load environment variables from .env file
 load_dotenv()
 
-
+df = pd.read_excel("challenge.xlsx", index_col=None)
+records = df.to_dict(orient="records")
+print(records)
 # Retrieve OpenAI API key
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"] 
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 async def execute_task():
-    """Executes the agent's task."""
-    agent = Agent(
-        #task="Go to https://www.youtube.com/@HarvestersTV, click on the first video and play it",
-        task="Go to Yahoo finace web site , search for trending tickers then extract all of them into CSV files and save.",
-        llm=ChatOpenAI(model="gpt-4o"),
-    )
-    try:
-        result = await agent.run()       
+    for record in records:
+        #print(record['First Name'])
+        task_description = ("Go to https://rpachallenge.com/ then map corresponding values into form fields "
+                            f"as follows: First Name as {record['First Name']}, Last Name as {record['Last Name']}, "
+                            f"Company Name as {record['Company Name']}, Role in Company as {record['Role in Company']}, "
+                            f"Address as {record['Address']}, Email as {record['Email']}, Phone Number as {record['Phone Number']}, "
+                            "then click submit ")
+        agent = Agent(
+            task=task_description,
+            llm=ChatOpenAI(model="gpt-4o"),
+        )
         
-        #print(result)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    try:
-        #result = await agent.run()
-        df = pd.read_json(result)
-        print("=====================",df)
-        df.to_csv('output.csv')
+        try:
+            result = await agent.run()
+            print(f"Task completed for {record['First Name']} {record['Last Name']}")
+            print(result)
+        except Exception as e:
+            print(f"An error occurred for {record['First Name']} {record['Last Name']}: {e}")
         
-        print(result)
-    except Exception as e:
-        print(f"An error occurred: {e}")    
 
 def main():
     """Main function."""
